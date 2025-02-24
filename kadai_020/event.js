618b9c33f4c4400d47997654209c87d41b434f81
amr161 <amr.181@icloud.com>
2025-02-24 10:59:51 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    const childList = document.createElement('h2')
+    childList.textContent = 'ボタンをクリックしました'　;
+  });
